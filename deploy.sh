ba73bf52b3f6a8274af4fd939966144586a25a3d
Punit Gour <punit.g237@gmail.com>
2021-03-25 18:50:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 fx=`cat cloud.deployment | grep $1`
 for filename in $fx
 do
-fx1=`echo $filename | cut -f1 -d ','`
-fx2=`echo $filename | cut -f2 -d ','`
-fx3=`echo $filename | cut -f3 -d ','`
+env=`echo $filename | cut -f1 -d ','`
+core=`echo $filename | cut -f2 -d ','`
+version=`echo $filename | cut -f3 -d ','`
 fx4=`echo $filename | cut -f4 -d ','`
 done
 
-sh 'anypoint-cli --username=$2 --password=$3 --environment=$1 runtime-mgr cloudhub-application modify --runtime "3.9.4" --workerSize $WORKERSIZE_ENV --property $PROPERTY_ENV $APP_NAME_ENV "/var/lib/jenkins/workspace/forevernew-wh-orders/target/wh_orders_process_api-1.0.0-SNAPSHOT.zip"'
+sh 'anypoint-cli --username=$2 --password=$3 --environment=$env runtime-mgr cloudhub-application modify --runtime $version --workerSize $WORKERSIZE_ENV --property $PROPERTY_ENV $APP_NAME_ENV "/var/lib/jenkins/workspace/forevernew-build-24/target/mulesoft-cicd-sample1-1.0.0-SNAPSHOT-mule-application.jar"'
