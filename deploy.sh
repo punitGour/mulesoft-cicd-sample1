57dcdfd823486566c50a44cbff7ae395a578f705
Punit Gour <punit.g237@gmail.com>
2021-03-25 18:54:05 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ version=`echo $filename | cut -f3 -d ','`
 fx4=`echo $filename | cut -f4 -d ','`
 done
 
-sh 'anypoint-cli --username=$2 --password=$3 --environment=$env runtime-mgr cloudhub-application modify --runtime $version --workerSize $WORKERSIZE_ENV --property $PROPERTY_ENV $APP_NAME_ENV "/var/lib/jenkins/workspace/forevernew-build-24/target/mulesoft-cicd-sample1-1.0.0-SNAPSHOT-mule-application.jar"'
+sh 'anypoint-cli --username="pradeepbo" --password="Cybage123" --environment=$env runtime-mgr cloudhub-application modify --runtime $version --workerSize $WORKERSIZE_ENV --property $PROPERTY_ENV $APP_NAME_ENV "/var/lib/jenkins/workspace/forevernew-build-24/target/mulesoft-cicd-sample1-1.0.0-SNAPSHOT-mule-application.jar"'
