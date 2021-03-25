b9c7aa573bf4bdcad49eaa06512bdb0717e07c24
Punit Gour <punit.g237@gmail.com>
2021-03-25 18:44:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,4 @@ fx3=`echo $filename | cut -f3 -d ','`
 fx4=`echo $filename | cut -f4 -d ','`
 done
 
-echo $fx1
-echo $fx2
-echo $fx3
-echo $fx4
+sh 'anypoint-cli --username=$2 --password=$3 --environment=$1 runtime-mgr cloudhub-application modify --runtime "3.9.4" --workerSize $WORKERSIZE_ENV --property $PROPERTY_ENV $APP_NAME_ENV "/var/lib/jenkins/workspace/forevernew-wh-orders/target/wh_orders_process_api-1.0.0-SNAPSHOT.zip"'
