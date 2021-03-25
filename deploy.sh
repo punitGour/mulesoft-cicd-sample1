e3cd347ca3e2ed867d5dc54fc8e2ba1b75ddec7e
Punit Gour <punit.g237@gmail.com>
2021-03-25 16:43:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
-fx= 'cat cloud.deployment | grep "DEV"'
-for filename in ${fx}
+fx=`cat cloud.deployment | grep "env"`
+for filename in $fx
 do
 fx1=`echo $filename | cut -f1 -d ','`
 fx2=`echo $filename | cut -f2 -d ','`
 fx3=`echo $filename | cut -f3 -d ','`
 fx4=`echo $filename | cut -f4 -d ','`
 done
-                    
+
 echo $fx1
 echo $fx2
 echo $fx3
