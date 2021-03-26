c4786a71a8496dd18dde2fb948cc2b7e8ff7287b
Punit Gour <punit.g237@gmail.com>
2021-03-26 19:58:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ env=`echo $filename | cut -f1 -d ','`
 core=`echo $filename | cut -f2 -d ','`
 version=`echo $filename | cut -f3 -d ','`
 property=`echo $filename | cut -f4 -d ','`
+AppName=`echo $filename | cut -f5 -d ','`
 done
 
 
@@ -17,4 +18,4 @@ echo $core
 echo $version
 echo $property
 
-anypoint-cli --username=$2 --password=$3 --environment=$env runtime-mgr cloudhub-application deploy --runtime $version --workerSize $core $4 "/var/lib/jenkins/workspace/forever-new/target/mulesoft-cicd-sample1-1.0.0-SNAPSHOT-mule-application.jar"||anypoint-cli --username=$2 --password=$3 --environment=$env runtime-mgr cloudhub-application modify --runtime $version --workerSize $core $4 "/var/lib/jenkins/workspace/forever-new/target/mulesoft-cicd-sample1-1.0.0-SNAPSHOT-mule-application.jar"
+anypoint-cli --username=$2 --password=$3 --environment=$env runtime-mgr cloudhub-application deploy --runtime $version --workerSize $core $4 "/var/lib/jenkins/workspace/forever-new/target/${AppName}"||anypoint-cli --username=$2 --password=$3 --environment=$env runtime-mgr cloudhub-application modify --runtime $version --workerSize $core $4 "/var/lib/jenkins/workspace/forever-new/target/${AppName}"
