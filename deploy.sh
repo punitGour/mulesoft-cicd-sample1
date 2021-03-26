a6a4aa6442b65a8ea5d36797aa40541e7680dcba
Punit Gour <punit.g237@gmail.com>
2021-03-26 19:07:56 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,11 @@ version=`echo $filename | cut -f3 -d ','`
 property=`echo $filename | cut -f4 -d ','`
 done
 
+FILE= "/var/lib/jenkins/workspace/forever-new/target/*.jar"
+
 echo $env
 echo $core
 echo $version
 echo $property
 
-anypoint-cli --username=$2 --password=$3 --environment=$env runtime-mgr cloudhub-application deploy --runtime $version --workerSize $core $4 "/var/lib/jenkins/workspace/forever-new/target/mulesoft-cicd-sample1-1.0.0-SNAPSHOT-mule-application.jar"||anypoint-cli --username=$2 --password=$3 --environment=$env runtime-mgr cloudhub-application modify --runtime $version --workerSize $core $4 "/var/lib/jenkins/workspace/forever-new/target/mulesoft-cicd-sample1-1.0.0-SNAPSHOT-mule-application.jar"
+anypoint-cli --username=$2 --password=$3 --environment=$env runtime-mgr cloudhub-application deploy --runtime $version --workerSize $core $4 $FILE||anypoint-cli --username=$2 --password=$3 --environment=$env runtime-mgr cloudhub-application modify --runtime $version --workerSize $core $4 $FILE
